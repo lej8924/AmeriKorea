eb2a50f70b7fefae0e2b89d678be5b025e71bcc0
lej8924 <eunjae8924@naver.com>
2024-07-31 11:23:25 +0900

    ➕ Init: 프로젝트 초기 세팅

---
+++ src/main/java/com/hana/amerikorea/AmeriKoreaApplication.java
@@ -0,0 +1,13 @@
+package com.hana.amerikorea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmeriKoreaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmeriKoreaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hana/amerikorea/AmeriKoreaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hana.amerikorea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmeriKoreaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
