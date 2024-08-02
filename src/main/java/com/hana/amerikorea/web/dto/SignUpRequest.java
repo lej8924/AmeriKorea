8ee41dcb774de6280c8b4928fc46702ff82f8c37
sewon <joosewon00@gmail.com>
2024-08-02 16:55:40 +0900

    add: dto ã코드추가

--- src/main/java/com/hana/amerikorea/web/dto/SignUpRequest.java
+++ src/main/java/com/hana/amerikorea/web/dto/SignUpRequest.java
@@ -1,8 +1,21 @@
 package com.hana.amerikorea.web.dto;
 
 public record SignUpRequest(
-        String id,
+        String name,
+        Boolean gender,
+        String email,
         String password,
-        String email
+        String password_check,
+        int birthday
 ) {
+    /*
+    name : Adam
+gender: man “bool”
+phone_number : 010-2421-1241 “int”
+email : dfdddda@gmail.com
+password : password1
+password_check : password1
+birthdate: 2000-01-01 “int” }
+     */
 }
+
