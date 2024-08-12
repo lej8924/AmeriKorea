54c23c8108d87eb908edb4c3762e26fbcd0f0e5c
sewon <joosewon00@gmail.com>
2024-08-12 17:19:34 +0900

    비밀번호찾기 화면 수정

--- src/main/java/com/hana/amerikorea/member/config/MailConfig.java
+++ src/main/java/com/hana/amerikorea/member/config/MailConfig.java
@@ -17,7 +17,7 @@ public class MailConfig {
 
         javaMailSender.setHost("smtp.gmail.com");
         javaMailSender.setUsername("tnals00718@gmail.com");
-        javaMailSender.setPassword("iwnqywblxcmfbmgy"); //앱 비번
+        javaMailSender.setPassword(""); //앱 비번 넣으셔야 합니당
         javaMailSender.setPort(587);
 
         javaMailSender.setJavaMailProperties(getMailProperties());
