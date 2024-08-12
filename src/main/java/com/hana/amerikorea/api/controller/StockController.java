7320d9ae9907da5b2dc3be48f389a34e64e41f4b
Revivekirin <kimwiz4352@gmail.com>
2024-08-12 16:38:07 +0900

    📝 StockController 수정

--- src/main/java/com/hana/amerikorea/api/controller/StockController.java
+++ src/main/java/com/hana/amerikorea/api/controller/StockController.java
@@ -1,94 +1,40 @@
 package com.hana.amerikorea.api.controller;
 
-import com.hana.amerikorea.api.service.CsvService;
-import com.hana.amerikorea.api.service.StockProcessor;
+import com.hana.amerikorea.api.service.AssetService;
+import com.hana.amerikorea.asset.dto.AssetDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
 @RestController
 public class StockController {
 
-    @Autowired
-    private CsvService csvService;
+    private final AssetService assetService;
 
     @Autowired
-    private StockProcessor stockProcessor;
-
-    @GetMapping("/getCurrentPrice")
-    public double getCurrentPrice(@RequestParam String stockName, @RequestParam boolean isKorean) {
-        String stockCode = csvService.getStockCode(stockName, isKorean);
-        if(stockCode.equals("종목코드를 못 찾았습니다")) {
-            throw new RuntimeException("Stock code not found for the given stock name");
-        }
-        return stockProcessor.getCurrentPrice(stockCode);
+    public StockController(AssetService assetService) {
+        this.assetService = assetService;
     }
 
-    @GetMapping("/getPurchasePrice")
-    public double getPurchasePrice(
+    @GetMapping("/getAssetData")
+    public AssetDTO getAssetData(
+            @RequestParam long assetID,
+            @RequestParam String assetName,
+            @RequestParam long assetAmount,
             @RequestParam String stockName,
-            @RequestParam boolean isKorean,
-            @RequestParam String purchaseDate
-    ) {
-        String stockCode = csvService.getStockCode(stockName, isKorean);
-        if(stockCode.equals("종목코드를 못 찾았습니다")) {
-            throw new RuntimeException("Stock code not found for the given stock name");
-        }
-        return stockProcessor.getPurchasePrice(stockCode, purchaseDate);
-    }
-    @GetMapping("/getCasHDividend")
-    public double getCashDividend(
-            @RequestParam String stockName,
+            @RequestParam String purchaseDate,
             @RequestParam boolean isKorean
-    ) {
-        String stockCode = csvService.getStockCode(stockName, isKorean);
-        if(stockCode.equals("종목코드를 못 찾았습니다")) {
-            throw new RuntimeException("Stock code not found for the given stock name");
+    ) throws IOException {
+        try {
+            return assetService.createAssetDTO(assetID, assetName, assetAmount, stockName, purchaseDate, isKorean);
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException("Error processing request", e);
         }
-        return stockProcessor.getCashDividend(stockCode);
     }
 
-    @GetMapping("/calculateInvestmentDividendRate") //투자배당률
-    public double calculateInvestmentDividendRate(
-            @RequestParam String stockName,
-            @RequestParam boolean isKorean,
-            @RequestParam String purchaseDate
-    ) {
-        String stockCode = csvService.getStockCode(stockName, isKorean);
-        Map<String, Double> stockData = new HashMap<>();
-        stockData.put("cashDividend", stockProcessor.getCashDividend(stockCode));
-        stockData.put("purchasePrice", stockProcessor.getPurchasePrice(stockCode, purchaseDate));
-        return stockProcessor.calculateInvestmentDividendRate(stockData);
-    }
-
-    @GetMapping("/calculateMarketDividendRate")
-    public double calculateMarketDividendRate(
-            @RequestParam String stockName,
-            @RequestParam boolean isKorean
-    ) {
-        String stockCode = csvService.getStockCode(stockName, isKorean);
-        Map<String, Double> stockData = new HashMap<>();
-        stockData.put("cashDividend", stockProcessor.getCashDividend(stockCode));
-        stockData.put("currentPrice", stockProcessor.getCurrentPrice(stockCode));
-        return stockProcessor.calculateMarketDividenRate(stockData);
-    }
-
-    @GetMapping("/calculateProfit")
-    public double calculateProfit(
-            @RequestParam String stockName,
-            @RequestParam boolean isKorean,
-            @RequestParam int amount,
-            @RequestParam String purchaseDate
-    ){
-        String stockCode = csvService.getStockCode(stockName, isKorean);
-        Map<String, Double> stockData = new HashMap<>();
-        stockData.put("currentPrice", stockProcessor.getCurrentPrice(stockCode));
-        stockData.put("purchasePrice", stockProcessor.getPurchasePrice(stockCode, purchaseDate));
-        return stockProcessor.calculateProfit(stockData, amount);
-    }
 
 }
