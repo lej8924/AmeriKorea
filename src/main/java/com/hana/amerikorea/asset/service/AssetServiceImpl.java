08f2aa5a7d78f4e6abc60381f71ed1c93c44249a
lej8924 <eunjae8924@naver.com>
2024-08-12 15:13:45 +0900

    🎨 Improve: AssetDomain과 Stock을 합침

--- src/main/java/com/hana/amerikorea/asset/service/AssetServiceImpl.java
+++ src/main/java/com/hana/amerikorea/asset/service/AssetServiceImpl.java
@@ -3,7 +3,6 @@ package com.hana.amerikorea.asset.service;
 import com.hana.amerikorea.asset.domain.AssetDomain;
 import com.hana.amerikorea.asset.dto.AssetDTO;
 import com.hana.amerikorea.asset.dto.AssetRepository;
-import com.hana.amerikorea.portfolio.domain.Stock;
 import com.hana.amerikorea.portfolio.repository.StockRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
